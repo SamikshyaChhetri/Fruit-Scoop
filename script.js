0b87df73c40938881c0a5c2e17211cb862b79610
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-21 23:10:52 +0545

    moved basket left and right

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 console.log(window.innerWidth);
 const basketImage = document.getElementById("basketImg");
 document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowLeft") {
-    let b = basketImage.style.left;
+  if (e.key === "ArrowRight") {
+    let b = parseInt(basketImage.style.left);
+    if (b <= 1420 - 250) {
+      basketImage.style.left = b + 10 + "px";
+    } else {
+      alert("Boundary reached");
+    }
+  } else if (e.key === "ArrowLeft") {
+    let b = parseInt(basketImage.style.left);
     console.log(b);
-    // if (b <= 1420) {
-    basketImage.style.left = b + 10 + "px";
-    // } else {
-    //   alert("Boundary reached");
-    // }
+    if (b >= 1420 - 1420) {
+      basketImage.style.left = b - 10 + "px";
+    } else {
+      alert("Boundary Reached");
+    }
   }
 });
