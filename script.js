6a9a8d62d8f90ce8580b4f061d27f99cc3080f94
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2024-07-21 23:44:08 +0545

    centered the basket for any size of screen

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 console.log(window.innerWidth);
+let w = window.innerWidth;
 const basketImage = document.getElementById("basketImg");
+let basketleft = (0 + w) / 2;
+console.log(basketleft);
+basketImage.style.left = basketleft - 120 + "px"; //15rem=240px so half of 240
+// window.location.reload();
 document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowRight") {
     let b = parseInt(basketImage.style.left);
     if (b <= 1420 - 250) {
       basketImage.style.left = b + 10 + "px";
-    } else {
-      alert("Boundary reached");
     }
   } else if (e.key === "ArrowLeft") {
     let b = parseInt(basketImage.style.left);
     console.log(b);
     if (b >= 1420 - 1420) {
       basketImage.style.left = b - 10 + "px";
-    } else {
-      alert("Boundary Reached");
     }
   }
 });
